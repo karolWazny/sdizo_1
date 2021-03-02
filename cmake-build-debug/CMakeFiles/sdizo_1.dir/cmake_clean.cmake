a4cf84b54a226947e10f3c3d51bce7c7cbe84f9a
karolWazny <karoljanwazny@gmail.com>
2021-03-02 16:35:39 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/sdizo_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdizo_1.dir/main.cpp.obj"
+  "sdizo_1.exe"
+  "sdizo_1.exe.manifest"
+  "sdizo_1.lib"
+  "sdizo_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdizo_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
